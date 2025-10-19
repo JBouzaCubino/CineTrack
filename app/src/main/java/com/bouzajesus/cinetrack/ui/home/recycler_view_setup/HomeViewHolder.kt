6b590e0f02d6549c0e7d5edf53299f655da8077c
Jesús <pilargn5@gmail.com>
2025-10-19 19:06:09 +0200

    init recycler view in HomeFragment.kt, updateList() created in HomeAdapter.kt and changes in HomeViewHolder.kt

--- app/src/main/java/com/bouzajesus/cinetrack/ui/home/recycler_view_setup/HomeViewHolder.kt
+++ app/src/main/java/com/bouzajesus/cinetrack/ui/home/recycler_view_setup/HomeViewHolder.kt
@@ -3,10 +3,14 @@ package com.bouzajesus.cinetrack.ui.home.recycler_view_setup
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.bouzajesus.cinetrack.data.remote.MediaItem
+import com.bouzajesus.cinetrack.databinding.HomeRecyclerViewItemBinding
 
 class HomeViewHolder(view: View): RecyclerView.ViewHolder(view) {
 
+    private val binding: HomeRecyclerViewItemBinding = HomeRecyclerViewItemBinding.bind(view)
+
     fun render(show: MediaItem){
 
+        binding.tvPrimaryTitle.text = show.primaryTitle
     }
 }
