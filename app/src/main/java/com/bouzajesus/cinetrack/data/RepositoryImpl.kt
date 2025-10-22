f2436a8954ac4d5216226af8a15ea50bf53b7f5c
Jesús <pilargn5@gmail.com>
2025-10-22 13:51:30 +0200

    ioexception created

--- app/src/main/java/com/bouzajesus/cinetrack/data/RepositoryImpl.kt
+++ app/src/main/java/com/bouzajesus/cinetrack/data/RepositoryImpl.kt
@@ -4,14 +4,21 @@ import com.bouzajesus.cinetrack.data.remote.MediaApiService
 import com.bouzajesus.cinetrack.data.remote.MediaDataModel
 import com.bouzajesus.cinetrack.domain.models.Media
 import com.bouzajesus.cinetrack.domain.repositories.Repository
+import okio.IOException
 import retrofit2.Response
 import javax.inject.Inject
 
 class RepositoryImpl @Inject constructor (private val mediaApiService: MediaApiService) : Repository {
 
-    override suspend fun getAllTitles(): List<Media> {
+    override suspend fun getAllTitles(): List<Media>{
         val response: Response<MediaDataModel> = mediaApiService.getTitles()
 
-        return response.body()?.toDomain() ?: emptyList()
+        if(response.isSuccessful){
+            return response.body()?.toDomain() ?: emptyList()
+        }
+        else{
+            throw IOException("${response.code()}: ${response.message()}")
+        }
+
     }
 }
