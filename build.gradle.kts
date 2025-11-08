3951dcba17205cf26c1e177fa08c7c68ea8d599e
Jesús <pilargn5@gmail.com>
2025-11-08 20:20:26 +0100

    safeargs plugin added

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,6 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     id("com.google.dagger.hilt.android") version "2.57.2" apply false
     id("com.google.devtools.ksp") version "2.2.20-2.0.4" apply false
+    id("androidx.navigation.safeargs.kotlin") version "2.9.6" apply false
+
 }
