3070dc8e9e3d9354ab10236ce4b743ae774b8530
Jesús <pilargn5@gmail.com>
2025-11-08 15:22:18 +0100

    DetailViewModel.kt implemented

--- app/src/main/java/com/bouzajesus/cinetrack/ui/details/DetailViewModel.kt
+++ app/src/main/java/com/bouzajesus/cinetrack/ui/details/DetailViewModel.kt
@@ -1,7 +1,37 @@
 package com.bouzajesus.cinetrack.ui.details
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.bouzajesus.cinetrack.domain.models.Category
+import com.bouzajesus.cinetrack.domain.usecases.ShowAllCategoriesUseCase
+import com.bouzajesus.cinetrack.ui.details.states.DetailUiState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import jakarta.inject.Inject
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import okio.IOException
 
-class DetailViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+@HiltViewModel
+class DetailViewModel @Inject constructor(
+    private val showAllCategoriesUseCase: ShowAllCategoriesUseCase
+) : ViewModel() {
+
+    private var _state: MutableStateFlow<DetailUiState> = MutableStateFlow(DetailUiState.Loading)
+    val state: StateFlow<DetailUiState> = _state
+
+    fun getCategories() {
+        viewModelScope.launch {
+            val listCategory: List<Category> = showAllCategoriesUseCase.execute()
+
+            try {
+                if (listCategory.isEmpty()) _state.value = DetailUiState.Error("lista vacía")
+                else _state.value = DetailUiState.Success(listCategory)
+            } catch (e: IOException) {
+                _state.value = DetailUiState.Error(e.message ?: "error desconocido")
+            }
+        }
+
+    }
+    
 }
