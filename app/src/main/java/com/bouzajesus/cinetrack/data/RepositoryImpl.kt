92c8dacbfaa91bf8e47c3a9f4332dbc1e13536cf
Jesús <pilargn5@gmail.com>
2025-11-08 15:20:56 +0100

    getCategories() function added and implemented

--- app/src/main/java/com/bouzajesus/cinetrack/data/RepositoryImpl.kt
+++ app/src/main/java/com/bouzajesus/cinetrack/data/RepositoryImpl.kt
@@ -1,28 +1,45 @@
 package com.bouzajesus.cinetrack.data
 
 import com.bouzajesus.cinetrack.data.remote.ApiService
+import com.bouzajesus.cinetrack.data.remote.models.CategoriesResponseModel
 import com.bouzajesus.cinetrack.data.remote.models.MediaDataResponseModel
+import com.bouzajesus.cinetrack.domain.models.Category
 import com.bouzajesus.cinetrack.domain.models.Media
 import com.bouzajesus.cinetrack.domain.repositories.Repository
 import okio.IOException
 import retrofit2.Response
 import javax.inject.Inject
 
-class RepositoryImpl @Inject constructor (private val mediaApiService: ApiService) : Repository {
+class RepositoryImpl @Inject constructor (private val apiService: ApiService) : Repository {
 
     override suspend fun getAllTitles(): List<Media>{
-        val response: Response<MediaDataResponseModel> = mediaApiService.getTitles()
+        val response: Response<MediaDataResponseModel> = apiService.getTitles()
 
         return responseCheck(response)
     }
 
     override suspend fun getShowByName(name: String): List<Media> {
-        val response: Response<MediaDataResponseModel> = mediaApiService.getTitleByName(name)
+        val response: Response<MediaDataResponseModel> = apiService.getTitleByName(name)
+
+        return responseCheck(response)
+    }
+
+    override suspend fun getCategories(): List<Category> {
+        val response = apiService.getInterests()
 
         return responseCheck(response)
     }
 
     private fun responseCheck(response: Response<MediaDataResponseModel>): List<Media>{
+        if(response.isSuccessful){
+            return response.body()?.toDomain() ?: emptyList()
+        }
+        else{
+            throw IOException("${response.code()}: ${response.message()}")
+        }
+    }
+
+    private fun responseCheck(response: Response<CategoriesResponseModel>): List<Category>{
         if(response.isSuccessful){
             return response.body()?.toDomain() ?: emptyList()
         }
--- app/src/main/java/com/bouzajesus/cinetrack/domain/repositories/Repository.kt
+++ app/src/main/java/com/bouzajesus/cinetrack/domain/repositories/Repository.kt
@@ -1,5 +1,6 @@
 package com.bouzajesus.cinetrack.domain.repositories
 
+import com.bouzajesus.cinetrack.domain.models.Category
 import com.bouzajesus.cinetrack.domain.models.Media
 
 interface Repository {
@@ -7,4 +8,6 @@ interface Repository {
     suspend fun getAllTitles(): List<Media>
 
     suspend fun getShowByName(name: String): List<Media>
+
+    suspend fun getCategories(): List<Category>
 }
