c6b2ff4825e0a5803d2c4fc1364cbff217fab73e
Jesús <pilargn5@gmail.com>
2025-11-10 11:21:14 +0100

    genres models created

--- app/src/main/java/com/bouzajesus/cinetrack/domain/models/Genres.kt
+++ app/src/main/java/com/bouzajesus/cinetrack/domain/models/Genres.kt
@@ -1,35 +1,32 @@
 package com.bouzajesus.cinetrack.domain.models
 
-import androidx.annotation.DrawableRes
-import androidx.annotation.StringRes
-import com.bouzajesus.cinetrack.R
-
-sealed class Genres(@StringRes val name:Int, @DrawableRes val image: Int) {
-    data object Action: Genres(R.string.action_genre,R.drawable.ic_action)
-    data object Adventure: Genres(R.string.adventure_genre,R.drawable.ic_adventure)
-    data object Anime: Genres(R.string.anime_genre,R.drawable.ic_anime)
-    data object Animation: Genres(R.string.animation_genre,R.drawable.ic_animation)
-    data object Biography: Genres(R.string.biography_genre,R.drawable.ic_biography)
-    data object Comedy: Genres(R.string.comedy_genre,R.drawable.ic_comedy)
-    data object Crime: Genres(R.string.crime_genre,R.drawable.ic_crime)
-    data object Documentary: Genres(R.string.documentary_genre,R.drawable.ic_documentary)
-    data object Drama: Genres(R.string.drama_genre,R.drawable.ic_drama)
-    data object Familiar: Genres(R.string.familiar_genre,R.drawable.ic_familiar)
-    data object Fantasy: Genres(R.string.fantasy_genre,R.drawable.ic_fantasy)
-    data object Gameshow: Genres(R.string.gameshow_genre,R.drawable.ic_gameshow)
-    data object History: Genres(R.string.history_genre,R.drawable.ic_history)
-    data object Horror: Genres(R.string.horror_genre,R.drawable.ic_horror)
-    data object Lifestyle: Genres(R.string.lifestyle_genre,R.drawable.ic_lifestyle)
-    data object Music: Genres(R.string.music_genre,R.drawable.ic_music)
-    data object Musical: Genres(R.string.musical_genre,R.drawable.ic_musical)
-    data object Mystery: Genres(R.string.mystery_genre,R.drawable.ic_mystery)
-    data object Reality: Genres(R.string.reality_genre,R.drawable.ic_reality)
-    data object Romance: Genres(R.string.romance_genre,R.drawable.ic_romance)
-    data object Scifi: Genres(R.string.scifi_genre,R.drawable.ic_scifi)
-    data object Short: Genres(R.string.short_genre,R.drawable.ic_short)
-    data object Sport: Genres(R.string.sport_genre,R.drawable.ic_sport)
-    data object Suspense: Genres(R.string.suspense_genre,R.drawable.ic_suspense)
-    data object Thriller: Genres(R.string.thriller_genre,R.drawable.ic_thriller)
-    data object War: Genres(R.string.war_genre,R.drawable.ic_war)
-    data object Western: Genres(R.string.western_genre, R.drawable.ic_western)
+enum class Genres {
+    Action,
+    Adventure,
+    Animation,
+    Anime,
+    Biography,
+    Comedy,
+    Crime,
+    Documentary,
+    Drama,
+    Familiar,
+    Fantasy,
+    Gameshow,
+    History,
+    Horror,
+    Lifestyle,
+    Music,
+    Musical,
+    Mystery,
+    Reality,
+    Romance,
+    Scifi,
+    Short,
+    Sport,
+    Suspense,
+    Thriller,
+    War,
+    Western,
+    Unknown
 }
---
+++ app/src/main/java/com/bouzajesus/cinetrack/ui/models/GenresModel.kt
@@ -0,0 +1,82 @@
+package com.bouzajesus.cinetrack.ui.models
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import com.bouzajesus.cinetrack.R
+import com.bouzajesus.cinetrack.domain.models.Genres
+
+sealed class GenresModel(genre: Genres, @StringRes val name: Int, @DrawableRes val image: Int) {
+    data object Action : GenresModel(Genres.Action, R.string.action_genre, R.drawable.ic_action)
+    data object Adventure : GenresModel(
+        Genres.Adventure,
+        R.string.adventure_genre,
+        R.drawable.ic_adventure
+    )
+
+    data object Anime : GenresModel(Genres.Anime, R.string.anime_genre, R.drawable.ic_anime)
+    data object Animation : GenresModel(
+        Genres.Animation,
+        R.string.animation_genre,
+        R.drawable.ic_animation
+    )
+
+    data object Biography : GenresModel(
+        Genres.Biography,
+        R.string.biography_genre,
+        R.drawable.ic_biography
+    )
+
+    data object Comedy : GenresModel(Genres.Comedy, R.string.comedy_genre, R.drawable.ic_comedy)
+    data object Crime : GenresModel(Genres.Crime, R.string.crime_genre, R.drawable.ic_crime)
+    data object Documentary : GenresModel(
+        Genres.Documentary,
+        R.string.documentary_genre,
+        R.drawable.ic_documentary
+    )
+
+    data object Drama : GenresModel(Genres.Drama, R.string.drama_genre, R.drawable.ic_drama)
+    data object Familiar : GenresModel(
+        Genres.Familiar,
+        R.string.familiar_genre,
+        R.drawable.ic_familiar
+    )
+
+    data object Fantasy : GenresModel(Genres.Fantasy, R.string.fantasy_genre, R.drawable.ic_fantasy)
+    data object Gameshow : GenresModel(
+        Genres.Gameshow,
+        R.string.gameshow_genre,
+        R.drawable.ic_gameshow
+    )
+
+    data object History : GenresModel(Genres.History, R.string.history_genre, R.drawable.ic_history)
+    data object Horror : GenresModel(Genres.Horror, R.string.horror_genre, R.drawable.ic_horror)
+    data object Lifestyle : GenresModel(
+        Genres.Lifestyle,
+        R.string.lifestyle_genre,
+        R.drawable.ic_lifestyle
+    )
+
+    data object Music : GenresModel(Genres.Music, R.string.music_genre, R.drawable.ic_music)
+    data object Musical : GenresModel(Genres.Musical, R.string.musical_genre, R.drawable.ic_musical)
+    data object Mystery : GenresModel(Genres.Mystery, R.string.mystery_genre, R.drawable.ic_mystery)
+    data object Reality : GenresModel(Genres.Reality, R.string.reality_genre, R.drawable.ic_reality)
+    data object Romance : GenresModel(Genres.Romance, R.string.romance_genre, R.drawable.ic_romance)
+    data object Scifi : GenresModel(Genres.Scifi, R.string.scifi_genre, R.drawable.ic_scifi)
+    data object Short : GenresModel(Genres.Short, R.string.short_genre, R.drawable.ic_short)
+    data object Sport : GenresModel(Genres.Sport, R.string.sport_genre, R.drawable.ic_sport)
+    data object Suspense : GenresModel(
+        Genres.Suspense,
+        R.string.suspense_genre,
+        R.drawable.ic_suspense
+    )
+
+    data object Thriller : GenresModel(
+        Genres.Thriller,
+        R.string.thriller_genre,
+        R.drawable.ic_thriller
+    )
+
+    data object War : GenresModel(Genres.War, R.string.war_genre, R.drawable.ic_war)
+    data object Western : GenresModel(Genres.Western, R.string.western_genre, R.drawable.ic_western)
+    data object Unknown : GenresModel(Genres.Unknown, R.string.unknown_genre, R.drawable.ic_unknown)
+}
